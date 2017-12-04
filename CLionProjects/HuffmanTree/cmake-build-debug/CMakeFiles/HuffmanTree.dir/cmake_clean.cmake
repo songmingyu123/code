acb1dbb5120dbc8df51719168fbbac3663f55486
songmingyu123 <498271160@qq.com>
2017-12-04 13:51:48 +0800

    huffman new project

---
+++ CLionProjects/HuffmanTree/cmake-build-debug/CMakeFiles/HuffmanTree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HuffmanTree.dir/main.c.o"
+  "HuffmanTree.pdb"
+  "HuffmanTree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HuffmanTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
